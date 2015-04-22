902381564aa424ae983b28e20061d48a870e74bd
Thomas Keitel <tom@undpaul.de>
2015-04-22 23:11:58 +0200

    Saved Drush site alias during command initialization for later use

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -7,9 +7,12 @@
 
 namespace hctom\DrupalUtils\Command;
 
+use hctom\DrupalUtils\Console\Application;
 use hctom\DrupalUtils\Drush\DrushSiteAliasAwareInterface;
 use hctom\DrupalUtils\Drush\DrushSiteAliasAwareTrait;
 use Symfony\Component\Console\Command\Command as SymfonyConsoleCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Drupal utilities command base class.
@@ -17,5 +20,13 @@ use Symfony\Component\Console\Command\Command as SymfonyConsoleCommand;
 abstract class Command extends SymfonyConsoleCommand implements DrushSiteAliasAwareInterface {
 
   use DrushSiteAliasAwareTrait;
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function initialize(InputInterface $input, OutputInterface $output) {
+    // Save Drush site alias.
+    $this->setDrushSiteAlias($input->getOption(Application::INPUT_OPTION_DRUSH_SITE_ALIAS));
+  }
 
 }
