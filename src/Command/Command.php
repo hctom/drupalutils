ae58950f88c439d1283f28ea734ee8ccc95caee1
Thomas Keitel <tom@undpaul.de>
2015-04-22 23:13:12 +0200

    Added runDrush() helper method to Command class

--- src/Command/Command.php
+++ src/Command/Command.php
@@ -8,9 +8,11 @@
 namespace hctom\DrupalUtils\Command;
 
 use hctom\DrupalUtils\Console\Application;
+use hctom\DrupalUtils\Drush\Drush;
 use hctom\DrupalUtils\Drush\DrushSiteAliasAwareInterface;
 use hctom\DrupalUtils\Drush\DrushSiteAliasAwareTrait;
 use Symfony\Component\Console\Command\Command as SymfonyConsoleCommand;
+use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
@@ -27,6 +29,33 @@ abstract class Command extends SymfonyConsoleCommand implements DrushSiteAliasAw
   protected function initialize(InputInterface $input, OutputInterface $output) {
     // Save Drush site alias.
     $this->setDrushSiteAlias($input->getOption(Application::INPUT_OPTION_DRUSH_SITE_ALIAS));
+  }
+
+  /**
+   * Run Drush command.
+   *
+   * @param string $drushCommandName
+   *   The name of the Drush command to execute.
+   * @param array $drushInputArray
+   *   The Drush input.
+   * @param OutputInterface $output
+   *   The output.
+   *
+   * @return int
+   *   0 if everything went fine, or an error code.
+   */
+  protected function runDrush($drushCommandName, array $drushInputArray, OutputInterface $output) {
+    // Extend input with static values.
+    $drushInputArray = array_merge(array(
+      'command' => $drushCommandName,
+      '--' . Application::INPUT_OPTION_DRUSH_SITE_ALIAS => $this->getDrushSiteAlias()
+    ), $drushInputArray);
+
+    // Instantiate Drush.
+    $drush = new Drush($this->getDrushSiteAlias());
+
+    return $drush
+      ->run(new ArrayInput($drushInputArray), $output);
   }
 
 }
