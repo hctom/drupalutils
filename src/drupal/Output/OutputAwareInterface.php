5c0f6e15ebd93181fd8a4814c24c2f99a60fbcf2
Thomas Keitel <tom@undpaul.de>
2015-05-10 21:30:10 +0200

    Added OutputAwareInterface and OutputAwareTrait

---
+++ src/drupal/Output/OutputAwareInterface.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * Contains hctom\DrupalUtils\Output\OutputAwareInterface.
+ */
+
+namespace hctom\DrupalUtils\Output;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * OutputAwareInterface should be implemented by classes that depend on the
+ * output.
+ */
+interface OutputAwareInterface {
+
+  /**
+   * Return output.
+   *
+   * @return OutputInterface
+   *   The output.
+   */
+  public function getOutput();
+
+  /**
+   * Set output.
+   *
+   * @param OutputInterface $output
+   *   The output.
+   *
+   * @return static
+   *   A self-reference for method chaining.
+   */
+  public function setOutput(OutputInterface $output);
+
+}
---
+++ src/drupal/Output/OutputAwareTrait.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Contains hctom\DrupalUtils\Output\OutputAwareTrait.
+ */
+
+namespace hctom\DrupalUtils\Output;
+
+use Symfony\Component\Console\Output\OutputInterface;
+
+/**
+ * OutputAwareTrait provides methods for working with the output.
+ */
+trait OutputAwareTrait {
+
+  /**
+   * The Output.
+   *
+   * @var OutputInterface
+   */
+  private $output;
+
+  /**
+   * Return output.
+   *
+   * @return OutputInterface
+   *   The output.
+   *
+   * @throws \RuntimeException
+   */
+  public function getOutput() {
+    if (!$this->output) {
+      throw new \RuntimeException('No output has been specified');
+    }
+
+    return $this->output;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function setOutput(OutputInterface $output) {
+    $this->output = $output;
+
+    return $this;
+  }
+
+}
