609b38544e3d9d921426c1a12ea42309bbd4bf39
Thomas Keitel <tom@undpaul.de>
2015-05-16 00:26:23 +0200

    Reworked Application class implementing new Helpers, structure etc.

--- src/drupal/Console/Application.php
+++ src/drupal/Console/Application.php
@@ -7,81 +7,236 @@
 
 namespace hctom\DrupalUtils\Console;
 
-use hctom\DrupalUtils\Command\Drush\HelpCommand;
-use hctom\DrupalUtils\Command\Drush\ListCommand;
+use hctom\DrupalUtils\Command\Command;
 use hctom\DrupalUtils\Command\Site\InstallSiteCommand;
+use hctom\DrupalUtils\Drush\SiteAliasAwareInterface;
+use hctom\DrupalUtils\Drush\SiteAliasConfig;
+use hctom\DrupalUtils\Console\Terminal\TerminalDimensionsAwareInterface;
 use hctom\DrupalUtils\Helper\DrupalHelper;
-use hctom\DrushWrapper\Helper\DrushHelper;
+use hctom\DrupalUtils\Helper\DrushHelper;
+use hctom\DrupalUtils\Helper\DrushProcessHelper;
+use hctom\DrupalUtils\Helper\DrushSiteAliasHelper;
+use hctom\DrupalUtils\Helper\FilesystemHelper;
+use hctom\DrupalUtils\Helper\FormatterHelper;
+use hctom\DrupalUtils\Log\Logger;
+use hctom\DrupalUtils\Log\LoggerInterface;
+use hctom\DrupalUtils\Output\OutputAwareInterface;
+use Monolog\Formatter\LineFormatter;
+use Monolog\Processor\PsrLogMessageProcessor;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
+use Symfony\Bridge\Monolog\Handler\ConsoleHandler;
 use Symfony\Component\ClassLoader\Psr4ClassLoader;
 use Symfony\Component\Console\Application as SymfonyConsoleApplication;
+use Symfony\Component\Console\Command\Command as SymfonyConsoleCommand;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
-use Symfony\Component\Console\Logger\ConsoleLogger;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
  * Drupal utilities application class.
  */
-class Application extends SymfonyConsoleApplication {
+class Application extends SymfonyConsoleApplication implements LoggerAwareInterface {
+
+  use LoggerAwareTrait;
 
   /**
-   * Add custom commands from Drush site alias configuration.
+   * Logo.
    *
-   * @param InputInterface $input
-   *   An Input instance.
-   * @param OutputInterface $output
-   *   An Output instance.
+   * @var string
    */
-  protected function addCustomCommands(InputInterface $input, OutputInterface $output) {
+  private static $logo =
+<<<EOT
+       __                            __        __   _  __
+  ____/ /_____ __  __ ____   ____ _ / /__  __ / /_ (_)/ /_____
+ / __  // ___// / / // __ \ / __ `// // / / // __// // // ___/
+/ /_/ // /   / /_/ // /_/ // /_/ // // /_/ // /_ / // /(__  )
+\__,_//_/    \__,_// .___/ \__,_//_/ \__,_/ \__//_//_//____/
+                  /_/
+
+EOT;
+
+  /**
+   * Add commands from Drush site alias configuration.
+   */
+  protected function addCommandsFromDrushSiteAliasConfig() {
     // Fetch Drush site alias details.
-    $siteAliasDetails = $this->getHelperSet()->get('drush')
-      ->setInput($input)
-      ->getSiteAliasDetails();
+    /* @var SiteAliasConfig $drushSiteAliasConfig */
+    $drushSiteAliasConfig = $this->getHelperSet()->get('drush_site_alias')
+      ->getConfig();
 
-    // Drupal utilities configuration found?
-    if (property_exists($siteAliasDetails, 'drupalutils') && property_exists($siteAliasDetails->drupalutils, 'commands')) {
-      // Command list is not an array?
-      if (!empty($siteAliasDetails->drupalutils->commands) && !is_array($siteAliasDetails->drupalutils->commands)) {
-        throw new \RuntimeException('Invalid command configuration found.');
+    if (!($commandConfig = $drushSiteAliasConfig->getCommands())) {
+      $this->getLogger()->debug('No commands registered in Drush site alias configuration');
+
+      return;
+    }
+
+    // Process command list.
+    foreach ($commandConfig as $commandName => $class) {
+      /* @var FormatterHelper $formatter */
+      $formatter = $this->getHelperSet()->get('formatter');
+
+      // Command class exists?
+      if (!class_exists($class)) {
+        throw new \RuntimeException('Registered command class not found: ' . $class);
       }
 
-      // Process command list.
-      foreach ($siteAliasDetails->drupalutils->commands as $commandName => $class) {
-        // Command class exists?
-        if (!class_exists($class)) {
-          throw new \RuntimeException('Command class not found: ' . $class);
-        }
+      /* @var Command $command */
+      $command = new $class();
 
-        $command = new $class();
-
-        // Log overridden command.
-        if ($this->has($command->getName())) {
-          $logger = new ConsoleLogger($output);
-          $logger->debug('Overridden command: ' . $command->getName() . ' => ' . $class);
-        }
-
-        // Add custom command.
-        $this->add($command);
+      // Command is not an instance of Symfony console command class?
+      if (!$command instanceof SymfonyConsoleCommand) {
+        throw new \RuntimeException('Registered invalid command class ' . $class . ' for ' . $commandName);
       }
+
+      // Log overridden command.
+      if ($this->has($command->getName())) {
+        $this->getLogger()
+          ->debug('<label>Registered overriding command:</label> {command} ==> {class}', array(
+            'command' => $formatter->formatCode($command->getName()),
+            'class' => $formatter->formatCode(ltrim($class, '\\')),
+          ));
+      }
+
+      // Log new command.
+      else {
+        $this->getLogger()
+          ->debug('<label>Registered new command:</label> {command} ==> {class}', array(
+            'command' => $formatter->formatCode($command->getName()),
+            'class' => $formatter->formatCode(ltrim($class, '\\')),
+          ));
+      }
+
+      // Add custom command.
+      $this->add($command);
     }
   }
 
   /**
    * {@inheritdoc}
    */
+  protected function configureIO(InputInterface $input, OutputInterface $output) {
+    parent::configureIO($input, $output);
+
+    // Configure output formatter styles.
+    $this->configureOutputFormatterStyles($output);
+
+    // Configure logger (if not already).
+    $this->configureLogger($output);
+
+    // Extend helpers.
+    $this->extendHelpers($input, $output);
+  }
+
+  /**
+   * Configure logger.
+   *
+   * @param OutputInterface $output
+   *   The Output.
+   */
+  protected function configureLogger(OutputInterface $output) {
+    // Create and set up logger (if not already).
+    if (!$this->getLogger()) {
+      // Create and set up line formatter (allowing inline line breaks).
+      $format = "%message%\n";
+      $formatter = new LineFormatter($format, NULL, TRUE);
+
+      // Create and set up console handler.
+      $handler = new ConsoleHandler($output, TRUE, array(
+        OutputInterface::VERBOSITY_NORMAL => Logger::ALWAYS,
+        OutputInterface::VERBOSITY_VERBOSE => Logger::NOTICE,
+        OutputInterface::VERBOSITY_VERY_VERBOSE => Logger::INFO,
+        OutputInterface::VERBOSITY_DEBUG => Logger::DEBUG,
+      ));
+      $handler->setFormatter($formatter);
+
+      // Create and set up logger.
+      $logger = new Logger($this->getName());
+      $logger->pushProcessor(new PsrLogMessageProcessor());
+      $logger->pushHandler($handler);
+
+      // Assign logger.
+      $this->setLogger($logger);
+    }
+  }
+
+  /**
+   * Configure output formatter styles.
+   *
+   * @param OutputInterface $output
+   *   An Output instance.
+   */
+  protected function configureOutputFormatterStyles(OutputInterface $output) {
+    // Code.
+    $output->getFormatter()->setStyle('code', new OutputFormatterStyle('cyan'));
+
+    // Code.
+    $output->getFormatter()->setStyle('counter', new OutputFormatterStyle('white', 'blue', array('bold')));
+
+    // Failure.
+    $output->getFormatter()->setStyle('failure', new OutputFormatterStyle('red', NULL));
+
+    // Label.
+    $output->getFormatter()->setStyle('label', new OutputFormatterStyle('white', NULL, array('bold')));
+
+    // Path.
+    $output->getFormatter()->setStyle('path', new OutputFormatterStyle('yellow'));
+
+    // Process output.
+    $output->getFormatter()->setStyle('processOutput', new OutputFormatterStyle('white'));
+
+    // Success.
+    $output->getFormatter()->setStyle('success', new OutputFormatterStyle('green'));
+  }
+
+  /**
+   * {@inheritdoc}
+   */
   public function doRun(InputInterface $input, OutputInterface $output) {
-    // Assign output to Drush helper.
-    $this->getHelperSet()->get('drush')
-      ->setOutput($output);
-
     // Initialize class loader.
-    $this->initializeClassLoader($input, $output);
+    $this->initializeClassLoader();
 
-    // Merge additional/overriding custom commands from Drush site alias
-    // configuration.
-    $this->addCustomCommands($input, $output);
+    // Merge additional/overriding commands from Drush site alias configuration.
+    $this->addCommandsFromDrushSiteAliasConfig();
 
     return parent::doRun($input, $output);
+  }
+
+  /**
+   * Extend helper objects.
+   *
+   * @param InputInterface $input
+   *   THe input.
+   * @param OutputInterface $output
+   *   The output.
+   */
+  protected function extendHelpers(InputInterface $input, OutputInterface $output) {
+    // Determine Drush site alias.
+    $drushSiteAlias = $this->getDrushSiteAliasFromInput($input);
+
+    // Extend helpers.
+    foreach ($this->getHelperSet() as $helper) {
+      // Inject Drush site alias into helper.
+      if ($helper instanceof SiteAliasAwareInterface) {
+        $helper->setSiteAlias($drushSiteAlias);
+      }
+
+      // Inject output into helper.
+      if ($helper instanceof OutputAwareInterface) {
+        $helper->setOutput($output);
+      }
+
+      // Inject logger into helper.
+      if ($helper instanceof LoggerAwareInterface) {
+        $helper->setLogger($this->getLogger());
+      }
+
+      // Inject terminal dimensions.
+      if ($helper instanceof TerminalDimensionsAwareInterface) {
+        $helper->setTerminalDimensions($this->getTerminalDimensions());
+      }
+    }
   }
 
   /**
@@ -90,14 +245,8 @@ class Application extends SymfonyConsoleApplication {
   protected function getDefaultCommands() {
     $defaultCommands = parent::getDefaultCommands();
 
-    // Drush command help.
-    $defaultCommands[] = new HelpCommand();
-
     // Install site.
     $defaultCommands[] = new InstallSiteCommand();
-
-    // List Drush commands.
-    $defaultCommands[] = new ListCommand();
 
     return $defaultCommands;
   }
@@ -114,6 +263,18 @@ class Application extends SymfonyConsoleApplication {
     // Drush helper.
     $helperSet->set(new DrushHelper());
 
+    // Drush process helper.
+    $helperSet->set(new DrushProcessHelper());
+
+    // Drush site alias helper.
+    $helperSet->set(new DrushSiteAliasHelper());
+
+    // File system helper.
+    $helperSet->set(new FilesystemHelper());
+
+    // Formatter helper.
+    $helperSet->set(new FormatterHelper());
+
     return $helperSet;
   }
 
@@ -123,52 +284,99 @@ class Application extends SymfonyConsoleApplication {
   protected function getDefaultInputDefinition() {
     $inputDefinition = parent::getDefaultInputDefinition();
 
-    // Drush site alias option.
+    // Add additional options.
     $inputDefinition->addOptions(array(
-      new InputOption('--simulate', NULL, InputOption::VALUE_NONE, "Simulate all relevant actions (don't actually change the system)."),
+      // Drush site alias.
       new InputOption('--site', NULL, InputOption::VALUE_REQUIRED, 'The Drush site alias to use.', '@none'),
     ));
 
     return $inputDefinition;
   }
 
+
   /**
-   * Initialize class loader.
+   * Return Drush site alias from input.
    *
    * @param InputInterface $input
-   *   An Input instance.
-   * @param OutputInterface $output
-   *   An output instance.
+   *   The input.
+   *
+   * @return string
+   *   The Drush site alias (defaults to '@none').
    */
-  protected function initializeClassLoader(InputInterface $input, OutputInterface $output) {
-    $logger = new ConsoleLogger($output);
+  protected function getDrushSiteAliasFromInput(InputInterface $input) {
+    $siteAlias = $siteAliasDefault = '@none';
 
-    // Fetch Drush site alias details.
-    $siteAliasDetails = $this->getHelperSet()->get('drush')
-      ->setInput($input)
-      ->getSiteAliasDetails();
-
-    // Drupal utilities configuration found?
-    if (property_exists($siteAliasDetails, 'drupalutils') && property_exists($siteAliasDetails->drupalutils, 'autoload')) {
-      // Command list is not an array?
-      if (!empty($siteAliasDetails->drupalutils->autoload) && !is_object($siteAliasDetails->drupalutils->autoload)) {
-        throw new \RuntimeException('Invalid autoloader configuration found.');
-      }
-
-      if (!property_exists($siteAliasDetails->drupalutils->autoload, 'psr-4') || count($siteAliasDetails->drupalutils->autoload) > 1) {
-        throw new \RuntimeException('Currently only the PSR-4 autoloading standard is supported.');
-      }
-
-      $autoLoader = new Psr4ClassLoader();
-      foreach ($siteAliasDetails->drupalutils->autoload as $autoLoadStandard => $autoLoadDefinitions) {
-        foreach ($autoLoadDefinitions as $key => $value) {
-          $autoLoader->addPrefix($key, $value);
-          $logger->debug('Registered namespace: ' . $key . ' => ' . $value);
-        }
-      }
-      $autoLoader->register();
+    // Inspect input for Drush site alias.
+    if ($input->hasOption('site')) {
+      $siteAlias = $input->getOption('site');
+    }
+    elseif ($input->hasParameterOption('--site')) {
+      $siteAlias = $input->getParameterOption('--site', $siteAliasDefault);
     }
 
+    return trim($siteAlias) ? trim($siteAlias) : $siteAliasDefault;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getHelp() {
+    return self::$logo . parent::getHelp();
+  }
+
+  /**
+   * Return logger.
+   *
+   * @return LoggerInterface
+   *   The logger.
+   */
+  public function getLogger() {
+    return $this->logger;
+  }
+
+  /**
+   * Initialize class loader.
+   */
+  protected function initializeClassLoader() {
+    /* @var FormatterHelper $formatter */
+    $formatter = $this->getHelperSet()->get('formatter');
+
+    /* @var SiteAliasConfig $drushSiteAliasConfig */
+    $drushSiteAliasConfig = $this->getHelperSet()->get('drush_site_alias')
+      ->getConfig();
+
+    // No class namespaces in Drush site alias configuration?
+    if (!($classLoaderConfig = $drushSiteAliasConfig->getClassLoaderNamespaces())) {
+      $this->getLogger()->debug('No class loader prefixes registered in Drush site alias configuration');
+
+      return;
+    }
+
+    // Initialize class loaders.
+    $classLoaders = array(
+      'psr-4' => new Psr4ClassLoader(),
+    );
+
+    // Register namespaces.
+    foreach ($classLoaders as $autoLoadStandard => $classLoader) {
+      switch ($autoLoadStandard) {
+        case 'psr-4':
+          if (($namespaces = $drushSiteAliasConfig->getPsr4ClassLoaderNamespaces())) {
+            foreach ($namespaces as $namespace => $path) {
+              $classLoader->addPrefix($namespace, $path);
+
+              $this->getLogger()
+                ->debug('<label>Registered PSR-4 class loader prefix:</label> {prefix} ==> {baseDir}', array(
+                  'prefix' => $formatter->formatCode(rtrim($namespace, '\\')),
+                  'baseDir' => $formatter->formatPath($path),
+                ));
+            }
+
+            $classLoader->register();
+          }
+          break;
+      }
+    }
   }
 
 }
