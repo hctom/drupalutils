2ba663370f0f7952b272dd6bd45a94f79dd62e85
Thomas Keitel <tom@undpaul.de>
2015-05-16 00:39:51 +0200

    Removed unnecessary use statement from FilesystemTask class

--- src/drupal/Task/FileSystem/FileSystemTask.php
+++ src/drupal/Task/FileSystem/FileSystemTask.php
@@ -8,7 +8,6 @@
 namespace hctom\DrupalUtils\Task\Filesystem;
 
 use hctom\DrupalUtils\Task\Task;
-use Symfony\Component\Filesystem\Filesystem;
 
 /**
  * Task command base class for file system operations.
