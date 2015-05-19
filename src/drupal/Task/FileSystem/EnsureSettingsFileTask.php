d698e3d37bac8c460b1f3a70a0d69ea78840b717
Thomas Keitel <tom@undpaul.de>
2015-05-20 00:03:28 +0200

    Added EnsureSettingsFileTask base class

---
+++ src/drupal/Task/FileSystem/EnsureSettingsFileTask.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * @file
+ * Contains hctom\DrupalUtils\Task\Filesystem\EnsureSettingsFileTask.
+ */
+
+namespace hctom\DrupalUtils\Task\Filesystem;
+
+/**
+ * Task command base class to ensure a settings file.
+ */
+abstract class EnsureSettingsFileTask extends EnsureFileTask {
+
+  /**
+   * Return path of directory containing settings files.
+   *
+   * @return string
+   *   The path of the directory containing all settings files.
+   */
+  protected function getSiteSettingsDirectory() {
+    return $this->getFilesystemHelper()->makePathAbsolute($this->getDrupalHelper()->getSiteDirectoryPath() . DIRECTORY_SEPARATOR . 'settings');
+  }
+
+}
