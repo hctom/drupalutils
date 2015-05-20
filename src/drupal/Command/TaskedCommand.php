04cd99b871ab2dd6391acc5d121b428ebae505ae
Thomas Keitel <tom@undpaul.de>
2015-05-21 00:52:24 +0200

    Added confirmation prompt to agree with task execution

--- src/drupal/Command/TaskedCommand.php
+++ src/drupal/Command/TaskedCommand.php
@@ -11,6 +11,7 @@ use hctom\DrupalUtils\Task\TaskInterface;
 use Symfony\Component\Console\Input\ArrayInput;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Question\ConfirmationQuestion;
 
 /**
  * Base class for all Drupal Utilities task commands.
@@ -37,50 +38,60 @@ abstract class TaskedCommand extends Command {
    *   NULL or 0 if everything went fine, or an error code.
    */
   public function executeTasks(InputInterface $input, OutputInterface $output) {
+    $formatter = $this->getFormatterHelper();
+
     // Determine task list.
     if (!($tasks = $this->getTasks())) {
       throw new \RuntimeException(sprintf('No tasks supplied for "%s"', $this->getName()));
     }
 
-    // Create and set up sub-application.
-    $subApp = clone $this->getApplication();
-    $subApp->setAutoExit(FALSE);
-    $subApp->addCommands($tasks);
-
     // Log table of contents.
     $this->logTableOfContents($tasks);
+    $this->getLogger()->always('');
 
-    // Run tasks.
-    $taskCount = 0;
-    foreach ($tasks as $task) {
-      // Does not implement task interface?
-      if (!$task instanceof TaskInterface) {
-        throw new \RuntimeException(sprintf('Invalid task class "%s" for "%s"', get_class($task), $task->getName()));
-      }
+    // Prompt if tasks should be executed.
+    $defaultValue = 'yes';
+    $question = new ConfirmationQuestion($formatter->formatQuestion('Execute tasks', $defaultValue), $defaultValue);
+    $continue = $this->getQuestionHelper()->ask($input, $output, $question);
 
-      // Increase task counter.
-      $taskCount++;
+    if ($continue) {
+      // Create and set up sub-application.
+      $subApp = clone $this->getApplication();
+      $subApp->setAutoExit(FALSE);
+      $subApp->addCommands($tasks);
 
-      // Find task command object.
-      $command = $subApp->find($task->getName());
+      // Run tasks.
+      $taskCount = 0;
+      foreach ($tasks as $task) {
+        // Does not implement task interface?
+        if (!$task instanceof TaskInterface) {
+          throw new \RuntimeException(sprintf('Invalid task class "%s" for "%s"', get_class($task), $task->getName()));
+        }
 
-      // Prepare task command arguments.
-      $parameters = array(
-        'command' => $task->getName(),
-      );
+        // Increase task counter.
+        $taskCount++;
 
-      // Output task information.
-      $this->getLogger()->always($this->getFormatterHelper()->formatTaskInfo($command, $taskCount, count($tasks)));
+        // Find task command object.
+        $command = $subApp->find($task->getName());
 
-      // Build command input.
-      $commandInput = new ArrayInput($parameters);
-      if ($input->getOption('no-interaction')) {
-        $commandInput->setInteractive(FALSE);
-      }
+        // Prepare task command arguments.
+        $parameters = array(
+          'command' => $task->getName(),
+        );
 
-      // Run task command and abort, if an error occurred.
-      if (($exitCode = $command->run($commandInput, $output))) {
-        return $exitCode;
+        // Output task information.
+        $this->getLogger()->always($formatter->formatTaskInfo($command, $taskCount, count($tasks)));
+
+        // Build command input.
+        $commandInput = new ArrayInput($parameters);
+        if ($input->getOption('no-interaction')) {
+          $commandInput->setInteractive(FALSE);
+        }
+
+        // Run task command and abort, if an error occurred.
+        if (($exitCode = $command->run($commandInput, $output))) {
+          return $exitCode;
+        }
       }
     }
   }
