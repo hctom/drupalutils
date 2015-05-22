2e66119fc6e9c305fb8b0324a4ca35bd4741dce4
Thomas Keitel <tom@undpaul.de>
2015-05-23 01:13:28 +0200

    Added DisableBartikThemeTask class

---
+++ src/drupal/Task/Theme/DisableBartikThemeTask.php
@@ -0,0 +1,39 @@
+<?php
+
+/**
+ * @file
+ * Contains hctom\DrupalUtils\Task\Theme\DisableBartikTheme.
+ */
+
+namespace hctom\DrupalUtils\Task\Theme;
+
+use hctom\DrupalUtils\Task\Project\DisableProjectTask;
+
+/**
+ * Provides a task command to disable the 'Bartik' theme.
+ */
+class DisableBartikThemeTask extends DisableProjectTask {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('task:theme:bartik:disable');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getProjectNames() {
+    return array('bartik');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return 'Disable "Bartik" theme';
+  }
+
+}
