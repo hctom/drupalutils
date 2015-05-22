abb19b5419c754b875eb24f7f44076933358532f
Thomas Keitel <tom@undpaul.de>
2015-05-22 21:45:49 +0200

    Reworked loading of Drupal version, public files directory and temp files directory in DrupalHelper class

--- src/drupal/Helper/DrupalHelper.php
+++ src/drupal/Helper/DrupalHelper.php
@@ -24,6 +24,34 @@ class DrupalHelper extends Helper implements LoggerAwareInterface, OutputAwareIn
   use OutputAwareTrait;
 
   /**
+   * Return Drupal core status information.
+   *
+   * @return \stdClass
+   *   An object containing Drupal core status information.
+   *
+   * @throws RuntimeException
+   */
+  protected function getCoreStatus() {
+    static $status;
+
+    if (!isset($status)) {
+      $process = $this->getDrushProcessHelper()
+        ->setCommandName('core-status')
+        ->setOptions(array(
+          'pipe' => TRUE,
+        ))
+        ->run(NULL, "Unable to determine Drupal's core status", FALSE);
+
+      // Error parsing core status.
+      if (($status = json_decode($process->getOutput())) === NULL) {
+        throw new RuntimeException("Unable to parse Drupal's core status");
+      }
+    }
+
+    return $status;
+  }
+
+  /**
    * Return Drush process helper.
    *
    * @return DrushProcessHelper
@@ -82,14 +110,13 @@ class DrupalHelper extends Helper implements LoggerAwareInterface, OutputAwareIn
     static $path;
 
     if (!isset($path)) {
-      $process = $this->getDrushProcessHelper()
-        ->setCommandName('core-status')
-        ->setOptions(array(
-          'fields' => 'files',
-          'field-labels' => '0',
-        ))
-        ->run(NULL, "Unable to determine Drupal's public files directory path", FALSE);
-      $path = trim($process->getOutput());
+      $path = NULL;
+      $status = $this->getCoreStatus();
+
+      // Contains public files directory path.
+      if (property_exists($status, 'files')) {
+        $path = $status->files;
+      }
     }
 
     if (!$path) {
@@ -132,20 +159,9 @@ class DrupalHelper extends Helper implements LoggerAwareInterface, OutputAwareIn
 
     if (!isset($path)) {
       $path = NULL;
+      $status = $this->getCoreStatus();
 
-      $process = $this->getDrushProcessHelper()
-        ->setCommandName('core-status')
-        ->setOptions(array(
-          'pipe' => TRUE,
-        ))
-        ->run(NULL, "Unable to determine Drupal's site directory path", FALSE);
-
-      // Error parsing core status.
-      if (($status = json_decode($process->getOutput())) === NULL) {
-        throw new RuntimeException('Unable to parse site directory path');
-      }
-
-      // Does not contain site directory path.
+      // Contains site directory path.
       if (property_exists($status, 'site')) {
         $path = $status->site;
       }
@@ -181,15 +197,13 @@ class DrupalHelper extends Helper implements LoggerAwareInterface, OutputAwareIn
     static $path;
 
     if (!isset($path)) {
-      $process = $this->getDrushProcessHelper()
-        ->setCommandName('core-status')
-        ->setOptions(array(
-          'fields' => 'temp',
-          'field-labels' => '0',
-        ))
-        ->run(NULL, "Unable to determine Drupal's temporary files directory path", FALSE);
+      $path = NULL;
+      $status = $this->getCoreStatus();
 
-      $path = trim($process->getOutput());
+      // Contains temporary files directory path.
+      if (property_exists($status, 'temp')) {
+        $path = $status->temp;
+      }
     }
 
     if (!$path) {
@@ -211,15 +225,13 @@ class DrupalHelper extends Helper implements LoggerAwareInterface, OutputAwareIn
     static $version;
 
     if (!isset($version)) {
-      $process = $this->getDrushProcessHelper()
-        ->setCommandName('core-status')
-        ->setOptions(array(
-          'fields' => 'drupal-version',
-          'field-labels' => '0',
-        ))
-        ->run(NULL, 'Unable to determine Drupal version', FALSE);
+      $version = NULL;
+      $status = $this->getCoreStatus();
 
-      $version = trim($process->getOutput());
+      // Contains version.
+      if (property_exists($status, 'drupal-version')) {
+        $version = $status->{'drupal-version'};
+      }
     }
 
     if (!$version) {
