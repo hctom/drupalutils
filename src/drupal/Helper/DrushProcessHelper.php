807aa1d27ea449e864ab6d3745966874527bf118
Thomas Keitel <tom@undpaul.de>
2015-05-23 00:11:49 +0200

    Allowed to set assumed answer to all prompts in DrushProcessHelper class

--- src/drupal/Helper/DrushProcessHelper.php
+++ src/drupal/Helper/DrushProcessHelper.php
@@ -9,6 +9,7 @@ namespace hctom\DrupalUtils\Helper;
 
 use hctom\DrupalUtils\Drush\SiteAliasAwareInterface;
 use hctom\DrupalUtils\Drush\SiteAliasAwareTrait;
+use Symfony\Component\Process\Exception\InvalidArgumentException;
 
 /**
  * Provides helpers to run external Drush processes.
@@ -18,15 +19,64 @@ class DrushProcessHelper extends ProcessHelper implements SiteAliasAwareInterfac
   use SiteAliasAwareTrait;
 
   /**
+   * Assumed answer: No.
+   */
+  const ASSUMED_ANSWER_NO = 'no';
+
+  /**
+   * Assumed answer: Yes.
+   */
+  const ASSUMED_ANSWER_YES = 'no';
+
+  /**
+   * Assumed answer to all prompt.
+   *
+   * @var string
+   */
+  private $assumedAnswerToAllPrompts;
+
+  /**
+   * Answer 'no' to all prompts.
+   *
+   * @return static
+   *   A self-reference for method chaining.
+   */
+  public function answerNoToAllPrompts() {
+    return $this->setAssumedAnswerToAllPrompts(static::ASSUMED_ANSWER_NO);
+  }
+
+  /**
+   * Answer 'yes' to all prompts.
+   *
+   * @return static
+   *   A self-reference for method chaining.
+   */
+  public function answerYesToAllPrompts() {
+    return $this->setAssumedAnswerToAllPrompts(static::ASSUMED_ANSWER_YES);
+  }
+
+  /**
    * {@inheritdoc}
    */
   protected function buildOptions() {
     $options = parent::buildOptions();
 
-    // Assume 'yes' as answer to all prompts.
-    $options['yes'] = '--yes';
+    // Assume 'yes' or 'no' as answer to all prompts.
+    $options[$this->getAssumedAnswerToAllPrompts()] = '--' . $this->getAssumedAnswerToAllPrompts();
 
     return $options;
+  }
+
+  /**
+   * Return assumed answer to all prompts.
+   *
+   * @return string
+   *   The assumed answer to all prompts. Possible values:
+   *     - static::ASSUMED_ANSWER_NO: No.
+   *     - static::ASSUMED_ANSWER_YES: Yes.
+   */
+  public function getAssumedAnswerToAllPrompts() {
+    return $this->assumedAnswerToAllPrompts ? $this->assumedAnswerToAllPrompts : static::ASSUMED_ANSWER_YES;
   }
 
   /**
@@ -50,6 +100,43 @@ class DrushProcessHelper extends ProcessHelper implements SiteAliasAwareInterfac
     ));
 
     return $processBuilder;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function reset() {
+    return parent::reset()
+      ->setAssumedAnswerToAllPrompts(static::ASSUMED_ANSWER_YES);
+  }
+
+  /**
+   * Set assumed answer to all prompts.
+   *
+   * @param string $answer
+   *   The assumed answer to all prompts. Possible values:
+   *     - static::ASSUMED_ANSWER_NO: No.
+   *     - static::ASSUMED_ANSWER_YES: Yes.
+   *
+   * @return static
+   *   A self-reference for method chaining.
+   *
+   * @throws InvalidArgumentException
+   */
+  public function setAssumedAnswerToAllPrompts($answer) {
+    $allowed = array(
+      static::ASSUMED_ANSWER_NO,
+      static::ASSUMED_ANSWER_YES,
+    );
+
+    // Assumed answer is not allowed.
+    if (!in_array($answer, $allowed)) {
+      throw new InvalidArgumentException(sprintf('Invalid assumed answer "%"', $answer));
+    }
+
+    $this->assumedAnswerToAllPrompts = $answer;
+
+    return $this;
   }
 
 }
