4f10e260fc7092789e93f304ebc8ab0cc575609f
Thomas Keitel <tom@undpaul.de>
2015-05-27 23:55:50 +0200

    Improved documentation of EnableProjectTask class

--- src/drupal/Task/Project/EnableProjectTask.php
+++ src/drupal/Task/Project/EnableProjectTask.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Provides a task command to enable modules/themes.
+ * Task base class for enabling modules/themes.
  */
 abstract class EnableProjectTask extends ProjectTask {
 
