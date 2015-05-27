2a9b68451afb62cb4524e91299aaa7d0454dfecc
Thomas Keitel <tom@undpaul.de>
2015-05-27 23:55:31 +0200

    Improved documentation of DisableProjectTask class

--- src/drupal/Task/Project/DisableProjectTask.php
+++ src/drupal/Task/Project/DisableProjectTask.php
@@ -11,7 +11,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 /**
- * Provides a task command to disable modules/themes.
+ * Task base class for disabling modules/themes.
  */
 abstract class DisableProjectTask extends ProjectTask {
 
