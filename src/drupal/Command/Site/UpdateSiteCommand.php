31bc72d21e642339a164053fd64dcdf775cc5dc7
Thomas Keitel <tom@undpaul.de>
2015-05-31 00:15:22 +0200

    Added UpdateSiteCommand class

---
+++ src/drupal/Command/Site/UpdateSiteCommand.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * @file
+ * Contains hctom\DrupalUtils\Command\Site\UpdateSiteCommand.
+ */
+
+namespace hctom\DrupalUtils\Command\Site;
+
+use hctom\DrupalUtils\Command\TaskedCommand;
+use hctom\DrupalUtils\Task\Database\EnsureDefaultDatabaseSettingsTask;
+use hctom\DrupalUtils\Task\Database\UpdateDatabaseTask;
+use hctom\DrupalUtils\Task\Environment\EnsureEnvSettingsTask;
+use hctom\DrupalUtils\Task\Environment\SymlinkEnvHtaccessTask;
+use hctom\DrupalUtils\Task\Environment\SymlinkEnvSettingsTask;
+use hctom\DrupalUtils\Task\Filesystem\EnsurePublicFilesDirectoryTask;
+use hctom\DrupalUtils\Task\Filesystem\EnsureSiteDirectoryTask;
+
+/**
+ * Provides a command to perform basic updates on a Drupal site.
+ */
+class UpdateSiteCommand extends TaskedCommand {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('site:update')
+      ->setDescription('Update a Drupal site.')
+      ->registerTasks(array(
+        new EnsureSiteDirectoryTask(),
+        new EnsureEnvSettingsTask(),
+        new SymlinkEnvHtaccessTask(),
+        new SymlinkEnvSettingsTask(),
+        new EnsureDefaultDatabaseSettingsTask(),
+        new EnsurePublicFilesDirectoryTask(),
+        new UpdateDatabaseTask(),
+      ))
+      ->setHelp(
+<<<EOT
+The <info>%command.name%</info> command performs updates on a Drupal site:
+
+<info>%command.full_name%</info>
+EOT
+      );
+  }
+
+}
