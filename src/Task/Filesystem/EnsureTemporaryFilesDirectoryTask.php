ef287743f92de31683f028f61cae918c36e94731
Thomas Keitel <tom@undpaul.de>
2015-06-05 18:17:16 +0200

    Added EnsureTemporaryFilesDirectoryTask class

---
+++ src/Task/Filesystem/EnsureTemporaryFilesDirectoryTask.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * @file
+ * Contains hctom\DrupalUtils\Task\Filesystem\EnsureTemporaryFilesDirectoryTask.
+ */
+
+namespace hctom\DrupalUtils\Task\Filesystem;
+
+/**
+ * Provides a task command to ensure the temporary files directory.
+ */
+class EnsureTemporaryFilesDirectoryTask extends EnsureDirectoryTask {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function configure() {
+    $this
+      ->setName('task:directory:ensure:tmp');
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  function getPath() {
+    return $this->getDrupalHelper()->getTemporaryFilesDirectoryPath();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function getTitle() {
+    return 'Ensure temporary files directory';
+  }
+
+}
