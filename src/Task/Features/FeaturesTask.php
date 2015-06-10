eac461c1497c5a2e1a61bd6a14a799717e01eff8
Thomas Keitel <tom@undpaul.de>
2015-06-11 00:05:38 +0200

    Added FeaturesTask::getRevertableFeatures() method

--- src/Task/Features/FeaturesTask.php
+++ src/Task/Features/FeaturesTask.php
@@ -8,11 +8,33 @@
 namespace hctom\DrupalUtils\Task\Features;
 
 use hctom\DrupalUtils\Task\Task;
+use Symfony\Component\Process\Exception\RuntimeException;
 
 /**
  * Task command base class for feature related tasks.
  */
 abstract class FeaturesTask extends Task {
+
+  /**
+   * Return revertable features.
+   *
+   * @return array
+   *   An array of names of features that need to be reverted.
+   *
+   * @throws RuntimeException
+   */
+  public function getRevertableFeatures() {
+    $process = $this->getDrushProcessHelper()
+      ->setCommandName('drupalutils-features-revertable')
+      ->mustRun(NULL, 'Unable to determine revertable features', FALSE);
+
+    // Unable to parse revertable features.
+    if (($features = json_decode($process->getOutput())) === NULL) {
+      throw new RuntimeException('Unable to parse revertable features');
+    }
+
+    return $features;
+  }
 
   /**
    * {@inheritdoc}
